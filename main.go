01a700f29b1b7b559e15ebd75d343bd03fc7963b
ReesvGupta <guptareesav16@gmail.com>
2025-04-14 13:02:05 +0600

    implement multiple HTTP servers with context-aware request handling

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
+
+type KeyServerAddrType string
+
+const keyServerAddr KeyServerAddrType = "serverAddr"
 
 // w is the responseWriter directly while,
 // r is a pointer to the http.Request
@@ -18,16 +23,21 @@ The http.ResponseWriter is an io.Writer, which means you can use anything capabl
 
 In this case, you’re using the io.WriteString function to write your response to the body.
 */
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")                     // this logs on to the server
-	fmt.Printf("this is the request object: %v\n", r) // this logs on to the server
-	io.WriteString(w, "this is my website")           // this writes the string to w
+	ctx := r.Context()
+
+	fmt.Printf("got / request\n", ctx.Value(keyServerAddr)) // this logs on to the server
+	fmt.Printf("this is the request object: %v\n", r)       // this logs on to the server
+	io.WriteString(w, "this is my website")                 // this writes the string to w
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")                          // same here as welsl
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path) //same here as well
-	fmt.Printf("this is the response Writer: %d\n", w)          //same here as well
+
+	ctx := r.Context()
+	fmt.Printf("got /hello request\n", ctx.Value(keyServerAddr)) //same here as well
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)  //same here as well
+	fmt.Printf("this is the response Writer: %d\n", w)           //same here as well
 }
 
 // What is the Default Server Multiplexer?
@@ -35,6 +45,8 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 
 // So internally, Go is doing something like:
 // --> http.DefaultServeMux.HandleFunc("/", getRoot)
+
+/*
 func main() {
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
@@ -50,6 +62,7 @@ func main() {
 		os.Exit(1)
 	}
 }
+*/
 
 // some important points to remember: -->
 
@@ -60,3 +73,88 @@ func main() {
 
 	Now we will create our own
 */
+
+/*
+****************************************************************
+****************************************************************
+**********MULTIPLEXING********REQUEST********HANDLERS***********
+****************************************************************
+****************************************************************
+ */
+
+/*
+	func main() {
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("/", getRoot)
+		mux.HandleFunc("/hello", getHello)
+
+		err := http.ListenAndServe(":3000", mux)
+
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server closed\n")
+		} else if err != nil {
+			fmt.Printf("error starting server: %s\n", err)
+			os.Exit(1)
+		}
+
+}
+*/
+func main() {
+	/*
+	****************************************************************
+	****************************************************************
+	******RUNNING******MULTIPLE******SERVERS******AT******ONCE******
+	****************************************************************
+	****************************************************************
+	 */
+
+	//  Sometimes you may want to customize how the server runs, or you may want to run multiple HTTP servers in the same program at once. For example, you may have a public website and a private admin website you want to run from the same program. Since you can only have one default HTTP server, you wouldn’t be able to do this with the default one.
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	serverOne := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	serverTwo := &http.Server{
+		Addr:    ":4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
+}
