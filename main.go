f14705df07f2ffa3eda2995e27ae697334eb34f4
ReesvGupta <guptareesav16@gmail.com>
2025-04-14 11:19:55 +0600

    initial commit: add basic HTTP server with root and hello handlers

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// w is the responseWriter directly while,
+// r is a pointer to the http.Request
+
+/*
+For now, in both of our HTTP handlers, you use fmt.Printf to print when a request comes in for the handler function, then you use the http.ResponseWriter to send some text to the response body.
+
+The http.ResponseWriter is an io.Writer, which means you can use anything capable of writing to that interface to write to the response body.
+
+In this case, you’re using the io.WriteString function to write your response to the body.
+*/
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")                     // this logs on to the server
+	fmt.Printf("this is the request object: %v\n", r) // this logs on to the server
+	io.WriteString(w, "this is my website")           // this writes the string to w
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")                          // same here as welsl
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path) //same here as well
+	fmt.Printf("this is the response Writer: %d\n", w)          //same here as well
+}
+
+// What is the Default Server Multiplexer?
+// In Go, http.HandleFunc(...) registers the handler on the default multiplexer (http.DefaultServeMux), which is a built-in router that maps paths to handler functions.
+
+// So internally, Go is doing something like:
+// --> http.DefaultServeMux.HandleFunc("/", getRoot)
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	fmt.Println("server is listening on port : 80")
+
+	err := http.ListenAndServe(":3333", nil) // The nil means: use the default ServeMux, which already knows the paths and handlers you registered.
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+// some important points to remember: -->
+
+/*
+	In this section we just created a default server and routed the requests using the default server multiplexer. This is fine for local development. Lekin, PROD mei gand Fat jaayegi.
+
+	Since the default mux is shared globally. If another package calls http.HandleFunc(...), it affects our app’s router too. That can lead to surprises.
+
+	Now we will create our own
+*/
